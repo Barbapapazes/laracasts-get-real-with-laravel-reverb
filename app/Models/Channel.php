bdb59d57b60bcf7adc7b687809ae5233505a7841
barbapapazes <e.soubiran25@gmail.com>
2024-11-23 11:16:08 +0100

    initial commit

---
+++ app/Models/Channel.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\MessageSent;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Database\Eloquent\BroadcastsEvents;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Broadcast;
+
+class Channel extends Model
+{
+    use HasFactory, BroadcastsEvents;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array<string>|bool
+     */
+    protected $guarded = [];
+
+    /**
+     * The users subscribed to the channel.
+     */
+    public function subscribers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'subscriptions')
+            ->withTimestamps();
+    }
+
+    /**
+     * The messages sent to the channel.
+     */
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    /**
+     * Determine whether the given user is subscribed to the channel.
+     */
+    public function isSubscribed(User $user): bool
+    {
+        return $this->subscribers->contains($user);
+    }
+
+    /**
+     * Get the channel's subscribers in random order.
+     */
+    public function getSubscribers(): Collection
+    {
+        return $this->subscribers()->inRandomOrder()->get();
+    }
+
+    /**
+     * Get the channel's messages with user.
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages()->with('user')->get();
+    }
+
+    /**
+     * Subscribe the given user to the channel.
+     */
+    public function subscribe(User $user): bool
+    {
+        return $this->subscribers()->attach($user) === null;
+    }
+
+    /**
+     * Send a message to the channel.
+     */
+    public function send(User $user, string $message): void
+    {
+        if (! $message) {
+            return;
+        }
+
+        $message = $this->messages()->create([
+            'user_id' => $user->id,
+            'content' => $message,
+            'sent_at' => now(),
+        ]);
+
+        Broadcast::private("channels.{$this->id}")
+            ->as('App\\Events\\MessageSent')
+            ->with([
+                'message' => $message->load('user'),
+            ])
+            ->send();
+    }
+
+    public function broadcastOn(string $event)
+    {
+        return match ($event) {
+            'created' => new PresenceChannel('workspace'),
+            default => []
+        };
+    }
+}
---
+++ routes/channels.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\Channel;
+use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::channel('channels.{channel}', function ($user, Channel $channel) {
+    return $channel->isSubscribed($user);
+});
+
+Broadcast::channel('workspace', function ($user) {
+    return [
+        'id' => $user->id,
+        'name' => $user->name,
+    ];
+});
